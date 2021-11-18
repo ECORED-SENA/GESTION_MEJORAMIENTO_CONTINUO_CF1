1c414a77d08f935375855cbc4f4e35e925f43c53
Mao <mao8a87@gmail.com>
2021-11-18 15:48:12 -0600

    ultimos cambios

--- src/config/global.js
+++ src/config/global.js
@@ -319,7 +319,7 @@ export default {
     ],
     gestoresRepositorio: [
       {
-        nombre: 'Milady Tatiana Villamil Castellanos',
+        nombre: 'Alvaro Andres Angarita Ramirez',
         cargo: 'Validación y vinculación en plataforma LMS',
         centro: 'Centro de Comercio y Servicios Regional Tolima',
       },
